c8e4f0af0bc148ef5821d427069901b7ecb587ad
bradodarb <bradodarb@hotmail.com>
2018-08-05 16:50:55 -0700

    completed readme

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='schleppy',
       version='0.1',
       description='Utilities for traversing and transforming data structures',
-      long_description='Collection of simple functions to ease the burden of reading and shaping data',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
